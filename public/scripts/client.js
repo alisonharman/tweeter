c476c563dda7372114d4017f831adaead96b4b64
Alison Harman <alison_harman@sympatico.ca>
2022-01-03 14:56:04 +0000

    enable refetch on tweets on submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,3 @@
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
@@ -67,7 +40,7 @@ $(document).ready(function () {
     // takes return value and appends it to the tweets container
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      $('#tweets-container').append($tweet);
+      $('#tweets-container').prepend($tweet);
     }
   };
   
@@ -92,8 +65,11 @@ $(document).ready(function () {
     }
 
     $.post('/tweets/', serializedData)
-      .then((resp) => {
-        console.log("Hey there!");
+      .then(() => {
+        loadTweets()
+      })
+      .catch(err => {
+        console.log(err.message)
       })
 
   })
@@ -110,7 +86,7 @@ $(document).ready(function () {
     })
   }
 
-  loadTweets();
+  //loadTweets();
 
   $("article").hover(function () {
     $(this).toggleClass('highlightBox')
