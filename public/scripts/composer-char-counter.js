af18df8427e8fe200cff4088a8b4f1f1ef71f680
Alison Harman <alison_harman@sympatico.ca>
2022-01-10 18:09:40 +0000

    address lint errors

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
   // --- our code goes here ---
   const counter = $("#new-tweet-counter");
 
-  $("#new-tweet-text").on( "input", function() {
+  $("#new-tweet-text").on("input", function() {
     // find number of characters inputted into textarea
     let characterCount = $(this).val().length;
     // number of lettersRemaining in tweet
-    let lettersRemaining = 140 - parseInt(characterCount);
+    let lettersRemaining = 140 - characterCount;
     // color text red if letterRemaining is negative, else text is default black
     if (lettersRemaining < 0) {
       counter.addClass("max-count-reached");
