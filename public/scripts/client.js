a7baf98d8a117f64f761317b5e093dfa3e270601
alisonharman <alison_harman@sympatico.ca>
2022-10-25 22:03:45 +0000

    add jquery actions to toggle new tweet form when click downarrows

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,12 +3,10 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
-
-  const createTweetElement = function(tweet) {
-
+$(document).ready(function () {
+  const createTweetElement = function (tweet) {
     // prevent XSS on the inputted tweet text
-    const escape = function(str) {
+    const escape = function (str) {
       let div = document.createElement("div");
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
@@ -27,7 +25,9 @@ $(document).ready(function() {
           ${safeHTML}
         </p>
         <footer class="tweet-footer">
-          <span class="tweet-footer-timestamp">${timeago.format(tweet.created_at)}</span> 
+          <span class="tweet-footer-timestamp">${timeago.format(
+            tweet.created_at
+          )}</span> 
           <div class="tweet-footer-icons">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -35,25 +35,25 @@ $(document).ready(function() {
           </div>
         </footer>
       </article>`;
-   
+
     return markup;
   };
 
-  const renderTweets = function(tweets) {
+  const renderTweets = function (tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
     for (const tweetObject of tweets) {
       const tweet = createTweetElement(tweetObject);
-      $('#tweets-container').prepend(tweet);
+      $("#tweets-container").prepend(tweet);
     }
   };
 
-  const form = $('#new-tweet-form');
-  form.on('submit', function(event) {
+  const form = $("#new-tweet-form");
+  form.on("submit", function (event) {
     event.preventDefault();
     // find the actual input that the user added
-    const input = $('#new-tweet-text').val();
+    const input = $("#new-tweet-text").val();
     const error = $("#new-tweet-error");
 
     if (input === "") {
@@ -63,40 +63,48 @@ $(document).ready(function() {
     }
 
     if (input.length > 140) {
-      error[0].textContent = "Too long! Plz rspct our arbitrary limit of 140 chars. #kthxbye.";
+      error[0].textContent =
+        "Too long! Plz rspct our arbitrary limit of 140 chars. #kthxbye.";
       error.slideDown("slow");
       return;
     }
-    
+
     const serializedData = $(this).serialize();
 
-    $.post('/tweets', serializedData)
-      .then(function() {
+    $.post("/tweets", serializedData)
+      .then(function () {
         // remove error message if showing
         $("#new-tweet-error").slideUp("slow");
         // reset form so the previous input goes away
         $("#new-tweet-text").val("");
         $("#new-tweet-counter").text(140);
         $("#tweets-container").empty();
+        $("section.new-tweet").hide();
         loadTweets();
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err.message);
       });
-
   });
 
-  const loadTweets = function() {
+  const loadTweets = function () {
     $.ajax({
       method: "GET",
       dataType: "json",
       url: "/tweets",
-      success: tweets => renderTweets(tweets),
-      error: error => console.log(error)
+      success: (tweets) => renderTweets(tweets),
+      error: (error) => console.log(error),
     });
   };
-    
+
   // so tweets will be retrieved when user first goes to page
   loadTweets();
 
+  const $newTweet = $("section.new-tweet");
+  const $elem = $("i");
+  $elem.on("click", () => {
+    $newTweet.slideToggle("fast");
+    $newTweet.toggleClass("hidden");
+    $("#new-tweet-text").focus();
+  });
 });
